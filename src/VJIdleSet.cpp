b304c881bf85534213ff9f626e29d294c3b8692c
csugrue <csugrue@csugrues-MacBook-Pro.local>
2017-11-14 08:13:22 +0100

    first commit

---
+++ src/VJIdleSet.cpp
@@ -0,0 +1,56 @@
+//
+//  VJIdleSet.c
+//  ofVJSet
+//
+//  Created by csugrue on 11/9/17.
+//
+
+#include "VJIdleSet.h"
+
+void VJIdleSet::setup(){
+    isPlaying = false;
+    title = "Idle";
+    circleSize = 20;
+}
+
+void VJIdleSet::update(){
+    
+    circleSize = sin( ofGetElapsedTimef() ) * 30;
+}
+
+void VJIdleSet::draw(){
+    //cout << "draw";
+    ofSetColor(244,66,190);
+    ofDrawEllipse( mouseX, ofGetHeight()/2, 50 + circleSize, 50 + circleSize);
+}
+
+void VJIdleSet::start(){
+    // sets up mouse move events
+    ofAddListener(ofEvents().mouseMoved, this, &VJIdleSet::mouseMoved);
+    ofAddListener(ofEvents().keyPressed, this, &VJIdleSet::keyPressed);
+    isPlaying = true;
+    
+    ofSetBackgroundColor(255);
+}
+
+void VJIdleSet::pause(){
+    ofRemoveListener(ofEvents().mouseMoved, this, &VJIdleSet::mouseMoved);
+    ofRemoveListener(ofEvents().keyPressed, this, &VJIdleSet::keyPressed);
+    isPlaying = false;
+}
+
+void VJIdleSet::mouseMoved(ofMouseEventArgs & mouse){
+    mouseX = mouse.x;
+    mouseY = mouse.y;
+}
+
+void VJIdleSet::keyPressed(ofKeyEventArgs & keyboard){
+    
+    int key = keyboard.key;
+     ofLog() << "key pressed: " << key;
+    
+    // don't use reserved keys : 0, D, A, K , E, S, I, Q, B
+    if( key == ' '){
+       
+    }
+}
---
+++ src/ofApp.cpp
@@ -0,0 +1,110 @@
+#include "ofApp.h"
+#include "VJIdleSet.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    
+    currentSet = IDLE;
+    
+    allSVJs.push_back( new VJIdleSet() ); // Idle
+    allSVJs.push_back( new BaseSet() ); // Title
+    allSVJs.push_back( new BaseSet() ); // Dasha
+    allSVJs.push_back( new BaseSet() ); // Amanda
+    allSVJs.push_back( new BaseSet() ); // Kris
+    allSVJs.push_back( new BaseSet() ); // Erica
+    allSVJs.push_back( new BaseSet() ); // Sanie
+    allSVJs.push_back( new BaseSet() ); // Bella
+    allSVJs.push_back( new BaseSet() ); // Qinqin
+    allSVJs.push_back( new BaseSet() ); // Ice
+    
+    for ( int i = 0; i< allSVJs.size(); i++ ) { allSVJs[i]->setup(); }
+    
+    ChangeSet(IDLE);
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    
+    allSVJs[currentSet]->update();
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    
+    allSVJs[currentSet]->draw();
+    
+}
+
+void ofApp::ChangeSet(VJSetState newSet){
+    
+    allSVJs[currentSet]->pause();
+    currentSet = newSet;
+    allSVJs[currentSet]->start();
+    ofLog() << "New Set: " << newSet << " " << allSVJs[currentSet]->title;
+
+}
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+    switch(key){
+        case 'D': ChangeSet(DASHA); break;
+        case 'A': ChangeSet(AMANDA); break;
+        case 'K': ChangeSet(KRIS); break;
+        case 'E': ChangeSet(ERICA); break;
+        case 'S': ChangeSet(SANIE); break;
+        case 'B': ChangeSet(BELLA); break;
+        case 'Q': ChangeSet(QINQIN); break;
+        case 'I': ChangeSet(ICE); break;
+        case '0': ChangeSet(IDLE); break;
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
