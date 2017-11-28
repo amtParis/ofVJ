15946075e97f5f7885dc77cbe3abea4b8d1705b6
csugrue <csugrue@csugrues-MacBook-Pro.local>
2017-11-28 09:24:16 +0100

    i forgot the particlesgit add src/IdleParticle.cpp

--- src/IdleParticle.h
+++ src/IdleParticle.h
@@ -5,9 +5,39 @@
 //  Created by csugrue on 11/16/17.
 //
 
-#ifndef IdleParticle_hpp
-#define IdleParticle_hpp
+#pragma once
 
-#include <stdio.h>
+#include "ofMain.h"
 
-#endif /* IdleParticle_hpp */
+enum particleMode{
+    PARTICLE_MODE_ATTRACT = 0,
+    PARTICLE_MODE_REPEL,
+    PARTICLE_MODE_NEAREST_POINTS,
+    PARTICLE_MODE_NOISE
+};
+
+class IdleParticle{
+    
+public:
+    
+    void setup();
+    void update();
+    void draw();
+    void drawTrails();
+    
+    ofPoint pos;
+    ofPoint vel;
+    ofPoint frc;
+    
+    float drag;
+    float uniqueVal;
+    float scale;
+    
+    particleMode mode;
+    vector <ofPoint> attractPoints;
+    int currentAttractor;
+    
+    vector<ofPoint> trails;
+    
+    
+};
