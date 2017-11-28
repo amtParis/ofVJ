74f688eaaee59a547eec803ee5e3697d0770d526
csugrue <csugrue@csugrues-MacBook-Pro.local>
2017-11-28 08:44:34 +0100

    update idle to add particles

---
+++ src/IdleParticle.cpp
@@ -0,0 +1,8 @@
+//
+//  IdleParticle.cpp
+//  ofVJ
+//
+//  Created by csugrue on 11/16/17.
+//
+
+#include "IdleParticle.hpp"
---
+++ src/IdleParticle.h
@@ -0,0 +1,13 @@
+//
+//  IdleParticle.hpp
+//  ofVJ
+//
+//  Created by csugrue on 11/16/17.
+//
+
+#ifndef IdleParticle_hpp
+#define IdleParticle_hpp
+
+#include <stdio.h>
+
+#endif /* IdleParticle_hpp */
